c4753df445f9fae7a5e1dfeb1de837190b29ff6b
aiden1321 <154081737+aiden1321@users.noreply.github.com>
2024-04-08 11:48:53 +0100

    Delete PLAYER-MODS/Quests/Personal Chest Mod directory

--- PLAYER-MODS/Quests/Personal Chest Mod/PersonalChest.c
+++
@@ -1,8 +0,0 @@
-//Use this to spawn a static chest.
-//Chest spawns in green mountain.
-//When you or someone else opens the chest it creates a json file inside PersonalChest folder
-//Use it with build loader or spawn through ur ini
-
-//Green mountain
-
-SpawnObject( "PC_CommonChest", "3696.373291 402.028625 5991.107422", "51.000000 0.000000 0.000000" );
